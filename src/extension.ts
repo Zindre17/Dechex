d230fd62225071866cffc7d871283870ea10001b
Sindre Aarstrand <zindre17@gmail.com>
2021-10-31 13:39:04 +0000

    Added new activity bar tree view

--- src/extension.ts
+++ src/extension.ts
@@ -5,22 +5,29 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "dechex" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('dechex.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from dechex!');
-	});
-
-	context.subscriptions.push(disposable);
+	vscode.window.registerTreeDataProvider(
+		'dechexmain',
+		new MyTreeDataProvider()
+	);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
+
+class MyTreeDataProvider implements vscode.TreeDataProvider<vscode.TreeItem>{
+
+	getTreeItem(element: vscode.TreeItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
+		return element;
+	}
+	getChildren(element?: vscode.TreeItem): vscode.ProviderResult<vscode.TreeItem[]> {
+		if (!element) {
+			return [new vscode.TreeItem("hello", vscode.TreeItemCollapsibleState.None)];
+		}
+		return [];
+	}
+}
